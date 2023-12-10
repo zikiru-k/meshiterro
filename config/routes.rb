ef7499c209fce27ad47ac6d124ea79a26648a99c
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2023-12-10 03:29:55 +0000

    [Finish] 4-10

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
-  resources :post_images, only: [:new, :index, :show]
+  resources :post_images, only: [:new, :create, :index, :show]
 #   get 'post_images/new'
 #   get 'post_images/index'
 #   get 'post_images/show'

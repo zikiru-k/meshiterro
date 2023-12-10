1ca19537ce74e153c2984f730a6f546f55838631
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2023-12-10 02:47:55 +0000

    [Finish] 4-9

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
 
+  resources :post_images, only: [:new, :index, :show]
+#   get 'post_images/new'
+#   get 'post_images/index'
+#   get 'post_images/show'
+
   get 'homes/about' =>  'homes#about', as: 'about'
 end
--- db/migrate/20231210022040_create_post_images.rb
+++ db/migrate/20231210022040_create_post_images.rb
@@ -1,7 +1,6 @@
 class CreatePostImages < ActiveRecord::Migration[6.1]
   def change
     create_table :post_images do |t|
-      t.integer :id
       t.string :shop_name
       t.text :caption
       t.integer :user_id

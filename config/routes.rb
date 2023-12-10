91269ef56b5f5118c9ec0d1ea3c910c9ded0cde0
zikiru-k <149685941+zikiru-k@users.noreply.github.com>
2023-12-10 13:44:09 +0000

    [Finish] 4-19

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about' =>  'homes#about', as: 'about'
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
